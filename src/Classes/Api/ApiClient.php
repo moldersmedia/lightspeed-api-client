c5c4254cbd1904d480a093062627c93b6bf3fbc1
Donny5300 <wavangrondelle@live.nl>
2018-04-10 19:02:10 +0200

    Make function makeRequest extendable

--- src/Classes/Api/ApiClient.php
+++ src/Classes/Api/ApiClient.php
@@ -291,7 +291,7 @@
          * @throws \GuzzleHttp\Exception\GuzzleException
          * @throws SupplierExistsException
          */
-        private function makeRequest($url, $method, $payload, $resource): Response
+        protected function makeRequest($url, $method, $payload, $resource): Response
         {
             $requestUrl = $this->makeRequestUrl($url);
 
